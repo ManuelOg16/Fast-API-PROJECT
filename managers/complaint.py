5432d50fbbd836f23bffcc675b06540db485bc9e
vigemi2@gmail.com <vigemi2@gmail.com>
2022-12-20 13:48:10 -0500

    adding file requietremnts.txt and modified manager*complaint change state for status

--- managers/complaint.py
+++ managers/complaint.py
@@ -9,12 +9,14 @@ class ComplaintManager:
         if user["role"] == RoleType.complainer:
             q = q.where(complaint.c.complainer_id == user["id"])
         elif user["role"] == RoleType.approver:
-            q = q.where(complaint.c.state == State.pending)
+            print(complaint.c.status)
+            q = q.where(complaint.c.status == State.pending)
         return await database.fetch_all(q)
 
     @staticmethod
     async def create_complaint(complaint_data, user):
         data = complaint_data.dict()
+        print(data)
         data["complainer_id"] = user["id"]
         id_ = await database.execute(complaint.insert().values(**data))
         return await database.fetch_one(complaint.select().where(complaint.c.id == id_))
